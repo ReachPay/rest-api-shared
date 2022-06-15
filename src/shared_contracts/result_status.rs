437ad93f5f64a4a21f14cbdbdfd48c9d145fc0a4
amigin <amigin@gmail.com>
2022-06-15 13:54:25 +0300

    ResultStatus::TokenIsExpired Added

--- src/shared_contracts/result_status.rs
+++ src/shared_contracts/result_status.rs
@@ -14,4 +14,7 @@ pub enum ResultStatus {
 
     #[http_enum_case(id="-3"; description="Invalid token")]
     InvalidToken = -3,
+
+    #[http_enum_case(id="-4"; description="Token is expired")]
+    TokenIsExpired = -4,
 }
