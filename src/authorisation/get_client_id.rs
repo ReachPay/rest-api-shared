227d7dac423741a80b7eda2c9da99f29459f2e22
amigin <amigin@gmail.com>
2022-06-21 23:54:39 +0300

    Optimized get client_id

--- src/authorisation/get_client_id.rs
+++ src/authorisation/get_client_id.rs
@@ -3,13 +3,13 @@ use my_http_server::{HttpContext, HttpFailResult};
 use crate::middlewares::KV_USER_ID;
 
 pub trait GetClientId {
-    fn get_client_id(&self) -> Result<String, HttpFailResult>;
+    fn get_client_id(&self) -> Result<&str, HttpFailResult>;
 }
 
 impl GetClientId for HttpContext {
-    fn get_client_id(&self) -> Result<String, HttpFailResult> {
+    fn get_client_id(&self) -> Result<&str, HttpFailResult> {
         if let Some(client_id) = self.request.get_key_value(KV_USER_ID) {
-            let result = String::from_utf8(client_id.to_vec()).unwrap();
+            let result = std::str::from_utf8(client_id).unwrap();
             return Ok(result);
         }
 
