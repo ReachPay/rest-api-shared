47076b4c1294eac7d5a2f22ca28c773958d47e6f
amigin <amigin@gmail.com>
2022-06-22 20:59:08 +0300

    Language ID is extended

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod authorisation;
-mod get_lang_id;
+pub mod lang_id;
 pub mod middlewares;
 pub mod request_extentions;
 pub mod session_token;
 pub mod shared_contracts;
-pub use get_lang_id::GetLangId;
