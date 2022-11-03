34381b3a309b9b274ffcf2d96b4a1289f7ecfa02
amigin <amigin@gmail.com>
2022-11-03 19:20:26 +0200

    0.3.1 - new version of controllers

--- src/shared_contracts/result_status.rs
+++ src/shared_contracts/result_status.rs
@@ -1,67 +1,67 @@
-use my_http_server_swagger::MyHttpStringEnum;
+use my_http_server_swagger::MyHttpIntegerEnum;
 use serde_repr::*;
 
-#[derive(Serialize_repr, Deserialize_repr, MyHttpStringEnum, Debug)]
+#[derive(Serialize_repr, Deserialize_repr, MyHttpIntegerEnum, Debug)]
 #[repr(i16)]
 pub enum ResultStatus {
-    #[http_enum_case(id="0"; description="Operations was successful")]
+    #[http_enum_case(id=0; description="Operations was successful")]
     Ok,
 
-    #[http_enum_case(id="-1"; description="User already Exists")]
+    #[http_enum_case(id=-1; description="User already Exists")]
     UserAlreadyExists = -1,
 
-    #[http_enum_case(id="-2"; description="Invalid Username or Password")]
+    #[http_enum_case(id=-2; description="Invalid Username or Password")]
     InvalidUserNameOrPassword = -2,
 
-    #[http_enum_case(id="-3"; description="Invalid token")]
+    #[http_enum_case(id=-3; description="Invalid token")]
     InvalidToken = -3,
 
-    #[http_enum_case(id="-4"; description="Token is expired")]
+    #[http_enum_case(id=-4; description="Token is expired")]
     TokenIsExpired = -4,
 
-    #[http_enum_case(id="-5"; description="Validation Error")]
+    #[http_enum_case(id=-5; description="Validation Error")]
     ValidationError = -5,
 
-    #[http_enum_case(id="-6"; description="Not enough funds")]
+    #[http_enum_case(id=-6; description="Not enough funds")]
     NotEnoughFunds = -6,
 
-    #[http_enum_case(id="-7"; description="Asset not found")]
+    #[http_enum_case(id=-7; description="Asset not found")]
     AssetNoFound = -7,
 
-    #[http_enum_case(id="-8"; description="Asset pair not found")]
+    #[http_enum_case(id=-8; description="Asset pair not found")]
     AssetPairNotFound = -8,
 
-    #[http_enum_case(id="-9"; description="No liquidity")]
+    #[http_enum_case(id=-9; description="No liquidity")]
     NoLiquidity = -9,
 
-    #[http_enum_case(id="-10"; description="Exchange by this pair is disabled")]
+    #[http_enum_case(id=-10; description="Exchange by this pair is disabled")]
     ExchangeByThisPairIsDisabled = -10,
 
-    #[http_enum_case(id="-11"; description="Order Not Found")]
+    #[http_enum_case(id=-11; description="Order Not Found")]
     OrderIsNotFound = -11,
 
-    #[http_enum_case(id="-12"; description="Order is already expired")]
+    #[http_enum_case(id=-12; description="Order is already expired")]
     OrderIsAlreadyExpired = -12,
 
-    #[http_enum_case(id="-13"; description="Order is paid")]
+    #[http_enum_case(id=-13; description="Order is paid")]
     OrderIsPaid = -13,
 
-    #[http_enum_case(id="-14"; description="Issuer and payer can not be the same")]
+    #[http_enum_case(id=-14; description="Issuer and payer can not be the same")]
     IssuerAndPayerCanNotBeTheSame = -14,
 
-    #[http_enum_case(id="-15"; description="Order payer is not set")]
+    #[http_enum_case(id=-15; description="Order payer is not set")]
     OrderPayerIsNotSet = -15,
 
-    #[http_enum_case(id="-16"; description="You are not the payer of the order")]
+    #[http_enum_case(id=-16; description="You are not the payer of the order")]
     YouAreNotThePayerOfTheOrder = -16,
 
-    #[http_enum_case(id="-17"; description="Crypto deposit operation is not supported")]
+    #[http_enum_case(id=-17; description="Crypto deposit operation is not supported")]
     CryptoDepositIsNotSupported = -17,
 
-    #[http_enum_case(id="-18"; description="Order is canceled")]
+    #[http_enum_case(id=-18; description="Order is canceled")]
     OrderIsCanceled = -18,
 
-    #[http_enum_case(id="-19"; description="Not merchant")]
+    #[http_enum_case(id=-19; description="Not merchant")]
     NotMerchant = -19,
 }
 
