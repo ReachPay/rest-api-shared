841e1f3907a53b68f9de639dcc7c30e110dd3f0f
amigin <amigin@gmail.com>
2023-01-05 13:01:20 +0200

    Added HttpStructure

--- src/middlewares/auth_fail_response.rs
+++ src/middlewares/auth_fail_response.rs
@@ -1,15 +1,70 @@
 use my_http_server::HttpFailResult;
-use my_http_server_swagger::{MyHttpIntegerEnum, MyHttpObjectStructure};
+use my_http_server_swagger::MyHttpIntegerEnum;
 use serde::{Deserialize, Serialize};
 use serde_repr::*;
 
-#[derive(Serialize, Deserialize, Debug, MyHttpObjectStructure)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct UnathorizedRequestResponse {
     pub reason: UnauthorizedReasonCode,
     pub message: String,
     #[serde(rename = "claim")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub claim: Option<String>,
+}
+
+impl UnathorizedRequestResponse {
+    pub fn get_http_data_structure(
+    ) -> my_http_server_controllers::controllers::documentation::data_types::HttpObjectStructure
+    {
+        use my_http_server_controllers::controllers::documentation::*;
+        data_types::HttpObjectStructure {
+            struct_id: "UnathorizedRequestResponse",
+            fields: vec![
+                data_types::HttpField::new(
+                    "reason",
+                    UnauthorizedReasonCode::get_data_type(),
+                    true,
+                    None,
+                ),
+                data_types::HttpField::new("message", String::get_data_type(), true, None),
+                data_types::HttpField::new("claim", String::get_data_type(), true, None),
+            ],
+        }
+    }
+}
+impl<'s> TryFrom<my_http_server::InputParamValue<'s>> for UnathorizedRequestResponse {
+    type Error = my_http_server::HttpFailResult;
+    fn try_from(value: my_http_server::InputParamValue) -> Result<Self, Self::Error> {
+        value.from_json()
+    }
+}
+impl my_http_server_controllers::controllers::documentation::DataTypeProvider
+    for UnathorizedRequestResponse
+{
+    fn get_data_type(
+    ) -> my_http_server_controllers::controllers::documentation::data_types::HttpDataType {
+        use my_http_server_controllers::controllers::documentation::*;
+        let mut __hos = data_types::HttpObjectStructure::new("UnathorizedRequestResponse");
+        __hos.fields.push(data_types::HttpField::new(
+            "reason",
+            UnauthorizedReasonCode::get_data_type(),
+            true,
+            None,
+        ));
+        __hos.fields.push(data_types::HttpField::new(
+            "message",
+            String::get_data_type(),
+            true,
+            None,
+        ));
+        __hos.fields.push(data_types::HttpField::new(
+            "claim",
+            String::get_data_type(),
+            true,
+            None,
+        ));
+        __hos.into_http_data_type_object()
+    }
 }
 
 impl UnathorizedRequestResponse {
