88bf08db605bc2848cb43000e0fb6656b1b1a4f2
amigin <amigin@gmail.com>
2024-01-23 00:40:30 +0200

    Fixes

--- src/middlewares/auth_middleware.rs
+++ src/middlewares/auth_middleware.rs
@@ -1,70 +1,16 @@
-use service_sdk::my_http_server::{
-    HttpContext, HttpFailResult, HttpOkResult, HttpPath, HttpRequestHeaders, HttpServerMiddleware,
-    HttpServerRequestFlow,
-};
-use service_sdk::rust_extensions::date_time::DateTimeAsMicroseconds;
+use my_http_server::*;
+use service_sdk::{my_http_server, my_no_sql_sdk::reader::MyNoSqlDataReaderTcp};
+use std::sync::Arc;
 
-use crate::session_token::{SessionToken, TokenKey};
-
-use super::UnauthorizedRequestResponse;
-
-const AUTH_HEADER: &str = "authorization";
+use super::{GetSessionToken, SessionEntity, TradingPlatformRequestCredentials};
 
 pub struct AuthMiddleware {
-    token_key: TokenKey,
-    ignore_full_paths: Option<Vec<HttpPath>>,
-    ignore_start_path: Option<Vec<HttpPath>>,
+    sessions_reader: Arc<MyNoSqlDataReaderTcp<SessionEntity>>,
 }
 
 impl AuthMiddleware {
-    pub fn new(token_key: TokenKey) -> Self {
-        Self {
-            token_key,
-            ignore_full_paths: None,
-            ignore_start_path: None,
-        }
-    }
-
-    pub fn path_is_ignored(&self, http_path: &HttpPath) -> bool {
-        if let Some(ref items) = self.ignore_full_paths {
-            for full_path_to_ignore in items {
-                if http_path.is_the_same_to(full_path_to_ignore) {
-                    return true;
-                }
-            }
-        }
-
-        if let Some(ref items) = self.ignore_start_path {
-            for start_path_to_ignore in items {
-                if http_path.is_starting_with(start_path_to_ignore) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    pub fn add_full_path_to_ignore(&mut self, path: &str) {
-        if self.ignore_full_paths.is_none() {
-            self.ignore_full_paths = Some(Vec::new());
-        }
-
-        self.ignore_full_paths
-            .as_mut()
-            .unwrap()
-            .push(HttpPath::from_str(path));
-    }
-
-    pub fn add_start_path_to_ignore(&mut self, path: &str) {
-        if self.ignore_start_path.is_none() {
-            self.ignore_start_path = Some(Vec::new());
-        }
-
-        self.ignore_start_path
-            .as_mut()
-            .unwrap()
-            .push(HttpPath::from_str(path));
+    pub fn new(sessions_reader: Arc<MyNoSqlDataReaderTcp<SessionEntity>>) -> Self {
+        Self { sessions_reader }
     }
 }
 
@@ -75,73 +21,25 @@ impl HttpServerMiddleware for AuthMiddleware {
         ctx: &mut HttpContext,
         get_next: &mut HttpServerRequestFlow,
     ) -> Result<HttpOkResult, HttpFailResult> {
-        if self.path_is_ignored(&ctx.request.http_path) {
+        let session_token = ctx.get_session_token();
+
+        if session_token.is_none() {
             return get_next.next(ctx).await;
         }
 
-        match ctx
-            .request
-            .get_headers()
-            .try_get_case_insensitive(AUTH_HEADER)
-        {
-            Some(header) => {
-                if let Some(session_token) = SessionToken::parse_from_token(
-                    std::str::from_utf8(extract_token(header.value)).unwrap(),
-                    &self.token_key,
-                ) {
-                    let now = DateTimeAsMicroseconds::now();
+        let token_entity = self
+            .sessions_reader
+            .get_entity(&SessionEntity::get_pk(), session_token.unwrap())
+            .await;
 
-                    if now.unix_microseconds >= session_token.get_expires_microseconds() {
-                        return Err(UnauthorizedRequestResponse::new(
-                            super::UnauthorizedReasonCode::SessionTokenIsExpired,
-                            "Session token is expired".to_string(),
-                            None,
-                        ));
-                    }
-
-                    ctx.credentials = Some(Box::new(session_token));
-                    return get_next.next(ctx).await;
-                } else {
-                    return Err(UnauthorizedRequestResponse::new(
-                        super::UnauthorizedReasonCode::InvalidSessionToken,
-                        "Invalid session token".to_string(),
-                        None,
-                    ));
-                }
-            }
-            None => {
-                return Err(UnauthorizedRequestResponse::new(
-                    super::UnauthorizedReasonCode::InvalidSessionToken,
-                    "No session token found".to_string(),
-                    None,
-                ));
-            }
+        if token_entity.is_none() {
+            return get_next.next(ctx).await;
         }
-    }
-}
 
-fn extract_token(src: &[u8]) -> &[u8] {
-    if src[6] == b' ' {
-        return &src[7..];
-    }
-    src
-}
+        ctx.credentials = Some(Box::new(TradingPlatformRequestCredentials::new(
+            token_entity.unwrap(),
+        )));
 
-#[cfg(test)]
-mod tests {
-    use crate::middlewares::auth_middleware::extract_token;
-
-    #[test]
-    fn test_extract_token() {
-        let src = b"Bearer 1234567890";
-        let result = extract_token(src);
-        assert_eq!(result, b"1234567890");
-    }
-
-    #[test]
-    fn test_extract_token_without_bearer() {
-        let src = b"1234567890";
-        let result = extract_token(src);
-        assert_eq!(result, b"1234567890");
+        get_next.next(ctx).await
     }
 }
--- src/middlewares/mod.rs
+++ src/middlewares/mod.rs
@@ -1,6 +1,16 @@
 mod auth_fail_response;
 mod auth_fail_response_factory;
+#[cfg(feature = "auth-middleware")]
 mod auth_middleware;
 pub use auth_fail_response::*;
 pub use auth_fail_response_factory::AuthFailResponseFactory;
-pub use auth_middleware::AuthMiddleware;
+#[cfg(feature = "auth-middleware")]
+pub use auth_middleware::*;
+mod session_entity;
+pub use session_entity::*;
+mod get_session_token;
+pub use get_session_token::*;
+#[cfg(feature = "auth-middleware")]
+mod request_creds;
+#[cfg(feature = "auth-middleware")]
+pub use request_creds::*;
